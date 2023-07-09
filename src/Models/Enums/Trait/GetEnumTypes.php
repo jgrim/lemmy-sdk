e22010bff5616137504aac3c703cd99fd3aad609
Jason Grim <jason@gr.im>
2023-07-08 22:19:22 -0400

    Type cleanup

--- src/Models/Enums/Trait/GetEnumTypes.php
+++ src/Models/Enums/Trait/GetEnumTypes.php
@@ -4,5 +4,8 @@ namespace JGrim\Lemmy\Sdk\Models\Enums\Trait;
 
 trait GetEnumTypes
 {
-
+    public static function getAllValues(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
 }
