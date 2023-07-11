b7105f6ba003a328cfe7cf9b30f7cf91314bc4cb
Jason Grim <jason@gr.im>
2023-07-11 02:44:22 -0400

    Fixes

--- src/Models/Responses/GetFederatedInstancesResponse.php
+++ src/Models/Responses/GetFederatedInstancesResponse.php
@@ -5,8 +5,10 @@ namespace JGrim\Lemmy\Sdk\Models\Responses;
 
 use JGrim\Lemmy\Sdk\Contracts\ResponseModel as Model;
 use JGrim\Lemmy\Sdk\Models\Data\FederatedInstances;
+use Sunrise\Hydrator\Annotation\Relationship;
 
 final class GetFederatedInstancesResponse implements Model
 {
+    #[Relationship(FederatedInstances::class)]
     public ?FederatedInstances $federated_instances = null;
 }
