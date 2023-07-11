e5e1cf536c65c94c98512ae300a8150505453e01
Jason Grim <jason@gr.im>
2023-07-11 02:52:12 -0400

    Fixes

--- resources/examples/modlog.php
+++ resources/examples/modlog.php
@@ -2,12 +2,8 @@
 declare(strict_types=1);
 
 use JGrim\Lemmy\Sdk\Client;
-use JGrim\Lemmy\Sdk\Hydrator;
 use JGrim\Lemmy\Sdk\Models\Forms\GetModLog;
-use JGrim\Lemmy\Sdk\Models\Forms\GetPersonDetails;
-use JGrim\Lemmy\Sdk\Models\Forms\Login;
 use JGrim\Lemmy\Sdk\Models\Responses\GetModlogResponse;
-use JGrim\Lemmy\Sdk\Models\Responses\LoginResponse;
 
 include_once '../../vendor/autoload.php';
 
--- src/Models/Data/Instance.php
+++ src/Models/Data/Instance.php
@@ -7,7 +7,7 @@ use JGrim\Lemmy\Sdk\Contracts\Model;
 
 final class Instance implements Model
 {
-    public string $id;
+    public int $id;
     public string $domain;
     public string $published;
     public ?string $updated = null;
